1fcddf5cc3fa2b9e1aba9278573af4fdfa840ecd
ellebelleboo <eklelindholm@gmail.com>
2024-11-20 18:09:47 +0200

    Add files via upload

---
+++ uppgifter/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Uppgifter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Your name: ");
+            string name = Console.ReadLine();
+            
+
+            int num1 = 1;
+            int num2 = 2;
+
+
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine(num1);
+            Console.WriteLine(num2);
+
+
+        }
+    }
+}
