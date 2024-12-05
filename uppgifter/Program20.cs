6fb17d857cc5998a759685b1357ec3105bb3f049
ellebelleboo <eklelindholm@gmail.com>
2024-12-05 10:17:17 +0200

    Add files via upload

---
+++ uppgifter/Program20.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uppgift20
+{
+    class Circle
+    {
+        public double Radius { get; set; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public double CalculateArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Circle circle = new Circle(1.5);
+
+            double area = circle.CalculateArea();
+            Console.WriteLine($"Area of the circle is: {area}");
+
+            Console.ReadLine();
+        }
+    }
+
+}
