2ee97d25e46cbd94d6f0e5083d2e9349dd8ffa14
ellebelleboo <eklelindholm@gmail.com>
2024-12-09 22:52:11 +0200

    Add files via upload

---
+++ uppgifter/Program23.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uppgift23
+{
+    internal class Program
+    {
+        //static void Main(string[] args)
+        //{
+
+        //    string input = "abc";
+        //    int result;
+         
+        //    try 
+        //    {
+        //        result = Convert.ToInt32(input);
+        //        Console.WriteLine($"Conversion succeeded: {result}");
+        //    }
+        //    catch (FormatException e)
+        //    { 
+        //        Console.WriteLine ($"conversion.failed: { e.Message}");
+        //    }
+
+
+        //}
+
+        static bool CustomTryParse(string input, out int result)
+        {
+
+            try
+            {
+                result = Convert.ToInt32(input);
+                return true;
+            }
+            catch (FormatException e)
+            {
+                result = 0;
+                return false;
+            }
+        }
+
+        static void UseCustomParse()
+        {
+            string input = "123";
+            if (CustomTryParse(input, out int result))
+            {
+                Console.WriteLine($"Conversion succeeded: {result}");
+            }
+            else
+            {
+                Console.WriteLine("Conversion failed");
+            }
+
+        }
+    }
+}
